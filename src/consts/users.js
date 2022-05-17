961a6277a71e08cd62048bb62b508338285cc4e2
Muhammad <mdzhanov@gmail.com>
2022-05-17 16:09:39 +0600

    new students

--- src/consts/users.js
+++ src/consts/users.js
@@ -7,7 +7,27 @@ export let USERS_DATA = [
     {
         name: 'Polina',
         userName: 'an4morph'
-    }
+    },
+    {
+        name: "Арген",
+        userName: "sairbekow"
+    },
+    {
+        name: "Талгат",
+        userName: "slice312"
+    },
+    {
+        name: "Байэл",
+        userName: "bael2018"
+    },
+    {
+        name: "Эржан",
+        userName: "Kallnur"
+    },
+    {
+        name: "Алтынбек",
+        userName: "AltynKasym"
+    },
 ]
 
 USERS_DATA = USERS_DATA.map((el,index)=>{
